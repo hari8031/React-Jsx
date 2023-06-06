ae3901e09c1a5d7510289b211c5fc7c2d946747a
Hari <thisismekrishnan@gmail.com>
2023-06-06 15:29:02 +0530

    creating a component step by step in app.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * *import the react and reactdom libraries
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDom from "react-dom/client";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * *get a reference to the div with  ID root
+ */
+
+const element = document.getElementById("root");
+
+/**
+ * *Tell React to take control of that element (root)
+ */
+
+const root = ReactDom.createRoot(element);
+
+/**
+ * *Create a component
+ */
+
+const App = () => <h1>Hi there</h1>;
+
+/**
+ * *show the component on the screen
+ */
+
+root.render(<App/>)
